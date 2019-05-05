02a8823b401c0a0ed6fce649ba1667e7bfac9bcb
Craig Doremus <cdoremus@gmail.com>
2019-05-05 12:03:46 -0400

    Fixed missing semicolons

--- cypress/integration/actions.spec.ts
+++ cypress/integration/actions.spec.ts
@@ -1,26 +1,26 @@
 
 context('Actions', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/actions')
-  })
+    cy.visit('https://example.cypress.io/commands/actions');
+  });
 
 
   it('.type() - type into a DOM element', () => {
     cy.get('.action-email')
-      .type('fake@email.com').should('have.value', 'fake@email.com')
+      .type('fake@email.com').should('have.value', 'fake@email.com');
 
     cy.get('.action-disabled')
       // Ignore error checking prior to type
       // like whether the input is visible or disabled
       .type('disabled error checking', { force: true })
-      .should('have.value', 'disabled error checking')
-  })
+      .should('have.value', 'disabled error checking');
+  });
 
   it('.focus() - focus on a DOM element', () => {
     // https://on.cypress.io/focus
     cy.get('.action-focus').focus()
       .should('have.class', 'focus')
-      .prev().should('have.attr', 'style', 'color: orange;')
-  })
+      .prev().should('have.attr', 'style', 'color: orange;');
+  });
 
-})
+});
